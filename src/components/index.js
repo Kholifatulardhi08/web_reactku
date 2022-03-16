fe52f1c390ef1bc5e2107ab2173a6c3c480ef0c7
brilliandyzufar <brilliandy12@gmail.com>
2022-03-16 17:53:20 +0700

    UI dan API
    Signed-off-by: brilliandyzufar <brilliandy12@gmail.com>

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Hasil from './Hasil'
+import ListCategories from './ListComponent'
+import NavbarComponent from './NavbarComponent'
+import Menus from './Menus'
+
+export {Hasil, ListCategories, Menus, NavbarComponent}
